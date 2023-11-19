3811c928f51705584621abbc51b14078fba8ac0e
jonathan <jonathanhinestroza87@gmail.com>
2023-11-19 17:18:01 -0500

    Step one (#1) project build

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Nav/>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.form-signin {
+  max-width: 330px;
+  padding: 1rem;
 }
 
-nav {
-  padding: 30px;
+.form-signin .form-floating:focus-within {
+  z-index: 2;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
 }
 </style>
+
+
+<script setup lang="ts">
+import Nav from "@/components/Nav.vue";
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="form-signin w-100 m-auto">
+    <form>
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+      <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+    </form>
+  </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
